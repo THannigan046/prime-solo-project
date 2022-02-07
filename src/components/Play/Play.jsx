c3036e6bc8a4c4020a9ce854dcc98497c31a121b
Toivo Hannigan <THannigan046@gmail.com>
2022-02-07 15:16:24 -0600

    got tone making sounds

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -1,11 +1,20 @@
+import * as Tone from 'tone'
+
 function Play() {
 
+    const synth = new Tone.MembraneSynth().toDestination()
 
+    const playSynth = () => {
+        synth.triggerAttackRelease("C2", "8n")
+    }
     
     return (
         <>
+
+
         <h1>Dammit bobby, play your dang synths</h1>
             <img src="https://art.ngfiles.com/images/1647000/1647974_thejudinator_synth-bobby.jpg?f1613569660"/>
+            <button id='button' onClick={playSynth}>suffer</button>
         </>
     )
 }
