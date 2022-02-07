6eca8ff7a949c186053891dc878b636877ced4ba
Toivo Hannigan <THannigan046@gmail.com>
2022-02-07 13:17:25 -0600

    created presets table

--- database.sql
+++ database.sql
@@ -8,3 +8,11 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "preset" (
+    "id" SERIAL PRIMARY KEY, 
+    "name" VARCHAR (200) UNIQUE NOT NULL,
+    "json_config" JSON, 
+    "state_config" VARCHAR (10000),
+    "user_id" int NOT NULL
+)
