c0d107b9285568f11367daa9bd093f53b7505b12
Toivo Hannigan <THannigan046@gmail.com>
2022-02-07 20:33:27 -0600

    accidentally made a kick drum

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -2,20 +2,20 @@ import * as Tone from 'tone'
 import {useEffect} from 'react'
 function Play() {
 
-    const synth = new Tone.MembraneSynth().toDestination()
 
-    //jotting down ideas as I go 
-    /* 
-    Could make a simple drum machine
-    */
+    const startTransport = () => {
+        const kick1 = new Tone.MembraneSynth().toDestination();
+        const seq = new Tone.Sequence((time, note) => {
+            kick1.triggerAttackRelease(note, 0.1, time);
+            // subdivisions are given as subarrays
+        }, [false, null, false, null, false, null, false, null,]).start(0);
+        Tone.Transport.start();
+    }
 
-    
-
-    /* const seq = new Tone.Sequence((time, note) => {
-        synth.triggerAttackRelease(note, 0.1, time);
-        // subdivisions are given as subarrays
-    }, ["C4", false,]).start(0); */
-    
+    const stopTransport = () => {
+        Tone.Transport.stop()
+    }
+   
     const playKick = () => {
         kick.start()
     }
@@ -31,6 +31,8 @@ function Play() {
     const playTom = () => {
         tom1.start()
     }
+    
+    
 
     const kick = new Tone.Player("https://tonejs.github.io/audio/drum-samples/CR78/kick.mp3").toDestination()
 
@@ -40,16 +42,54 @@ function Play() {
 
     const tom1 = new Tone.Player("https://tonejs.github.io/audio/drum-samples/CR78/tom1.mp3").toDestination()
     
+    /* const drumSet = new Tone.Players({
+        urls: {
+            0: "kick.mp3",
+            1: "snare.mp3",
+            2: "hihat.mp3", 
+            3: "tom1.mp3",
+        },
+        fadeOut: "16n",
+        baseUrl: "https://tonejs.github.io/audio/drum-samples/CR78/"
+    }).toDestination() */
+
     return (
         <>
 
 
         <h1>Dammit bobby, play your dang synths</h1>
             <img src="https://art.ngfiles.com/images/1647000/1647974_thejudinator_synth-bobby.jpg?f1613569660"/>
+            <button onClick={startTransport}>start</button>
+            <button onClick={stopTransport}>stop</button>
+            <br></br>
             <button onClick={playKick}>Kick</button>
             <button onClick={playSnare}>Snare</button>
             <button onClick={playHat}>Hihat</button>
             <button onClick={playTom}>Tom1</button>
+            <br></br>
+            <p>kick</p>
+            <input  type="checkbox"/>
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <br></br>
+            <p>snare</p>
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <br></br>
+            <p>hihat</p>
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <br></br>
+            <p>tom1</p>
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
             
         </>
     )
