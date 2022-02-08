0b1d799a43942402d5538bdbf74c4673d8df1ecc
Toivo Hannigan <THannigan046@gmail.com>
2022-02-08 09:55:14 -0600

    working on setting notes but selector won't set state

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -1,15 +1,43 @@
 import * as Tone from 'tone'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 function Play() {
+    let notes = [step1, "C4", "C5", "C4",]
+    console.log(step1);
+    const [step1, setStep1] = useState("C3")
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [playButtonText, setPlayButtonText] = useState('play')
+    let [bpm, setBpm] = useState(120)
+    Tone.Transport.bpm.value = bpm
+
+    const volumeNode = new Tone.Volume(-9).toDestination();
+    const synth = new Tone.MonoSynth({
+        oscillator: {
+            //can be square, tri or saw, 
+            //gonna add a selector shortly
+            type: 'square'
+        }, 
+        filter: {
+            frequency: 18000,
+            type: 'lowpass'
+        }
+
+    }).chain(volumeNode, Tone.Destination)
+
+    
+
+    const timeSequence = new Tone.Sequence((time, note) => {
+        synth.triggerAttackRelease(note, 0.1, time)
+
+    }, notes ) 
 
 
     const startTransport = () => {
-        const kick1 = new Tone.MembraneSynth().toDestination();
-        const seq = new Tone.Sequence((time, note) => {
-            kick1.triggerAttackRelease(note, 0.1, time);
-            // subdivisions are given as subarrays
-        }, [false, null, false, null, false, null, false, null,]).start(0);
+        
+        Tone.start() // start tone audio context on user interaction per spec of web audio api
+        // !START! 
         Tone.Transport.start();
+        //seq.start()
+        timeSequence.start()
     }
 
     const stopTransport = () => {
@@ -59,14 +87,56 @@ function Play() {
 
         <h1>Dammit bobby, play your dang synths</h1>
             <img src="https://art.ngfiles.com/images/1647000/1647974_thejudinator_synth-bobby.jpg?f1613569660"/>
-            <button onClick={startTransport}>start</button>
+            <br></br>
+            <p>transport</p>
+            
+            <select 
+            name="step1" id="step1"
+            value={step1} onChange={evt => (setStep1(evt.target.value))}>
+            <option value="C3">C3</option>
+            <option value="D3">D3</option>
+            <option value="E3">E3</option>
+            <option value="G3">G3</option>
+            <option value="A4">A4</option>
+            <option value="C4">C4</option>
+            </select>
+            <select
+                name="step2" id="step2">
+                <option value="C3">C3</option>
+                <option value="D3">D3</option>
+                <option value="E3">E3</option>
+                <option value="G3">G3</option>
+                <option value="A4">A4</option>
+                <option value="C4">C4</option>
+            </select>
+            <select
+                name="step3" id="step3">
+                <option value="C3">C3</option>
+                <option value="D3">D3</option>
+                <option value="E3">E3</option>
+                <option value="G3">G3</option>
+                <option value="A4">A4</option>
+                <option value="C4">C4</option>
+            </select>
+            <select
+                name="step4" id="step4">
+                <option value="C3">C3</option>
+                <option value="D3">D3</option>
+                <option value="E3">E3</option>
+                <option value="G3">G3</option>
+                <option value="A4">A4</option>
+                <option value="C4">C4</option>
+            </select>
+            <button onClick={startTransport}>{playButtonText}</button>
             <button onClick={stopTransport}>stop</button>
             <br></br>
+            <p>drums</p>
             <button onClick={playKick}>Kick</button>
             <button onClick={playSnare}>Snare</button>
             <button onClick={playHat}>Hihat</button>
             <button onClick={playTom}>Tom1</button>
             <br></br>
+            <div className='drumSequencer'>
             <p>kick</p>
             <input  type="checkbox"/>
             <input type="checkbox" />
@@ -90,6 +160,7 @@ function Play() {
             <input type="checkbox" />
             <input type="checkbox" />
             <input type="checkbox" />
+            </div>
             
         </>
     )
