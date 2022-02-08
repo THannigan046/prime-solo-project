5ea57675429586c499fe1577a78e7c7b26c397bd
Toivo Hannigan <THannigan046@gmail.com>
2022-02-07 18:15:25 -0600

    drum buttons working

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -1,12 +1,44 @@
 import * as Tone from 'tone'
-
+import {useEffect} from 'react'
 function Play() {
 
     const synth = new Tone.MembraneSynth().toDestination()
 
-    const playSynth = () => {
-        synth.triggerAttackRelease("C2", "8n")
+    //jotting down ideas as I go 
+    /* 
+    Could make a simple drum machine
+    */
+
+    
+
+    /* const seq = new Tone.Sequence((time, note) => {
+        synth.triggerAttackRelease(note, 0.1, time);
+        // subdivisions are given as subarrays
+    }, ["C4", false,]).start(0); */
+    
+    const playKick = () => {
+        kick.start()
     }
+
+    const playSnare = () => {
+        snare.start()
+    }
+
+    const playHat = () => {
+        hihat.start()
+    }
+
+    const playTom = () => {
+        tom1.start()
+    }
+
+    const kick = new Tone.Player("https://tonejs.github.io/audio/drum-samples/CR78/kick.mp3").toDestination()
+
+    const snare = new Tone.Player("https://tonejs.github.io/audio/drum-samples/CR78/snare.mp3").toDestination()
+
+    const hihat = new Tone.Player("https://tonejs.github.io/audio/drum-samples/CR78/hihat.mp3").toDestination()
+
+    const tom1 = new Tone.Player("https://tonejs.github.io/audio/drum-samples/CR78/tom1.mp3").toDestination()
     
     return (
         <>
@@ -14,7 +46,11 @@ function Play() {
 
         <h1>Dammit bobby, play your dang synths</h1>
             <img src="https://art.ngfiles.com/images/1647000/1647974_thejudinator_synth-bobby.jpg?f1613569660"/>
-            <button id='button' onClick={playSynth}>suffer</button>
+            <button onClick={playKick}>Kick</button>
+            <button onClick={playSnare}>Snare</button>
+            <button onClick={playHat}>Hihat</button>
+            <button onClick={playTom}>Tom1</button>
+            
         </>
     )
 }
