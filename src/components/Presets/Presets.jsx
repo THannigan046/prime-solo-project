09024cd8a3d8c9adbf282721d78e2f538862d509
Toivo Hannigan <THannigan046@gmail.com>
2022-02-09 15:03:16 -0600

    starting delete route

--- src/components/Presets/Presets.jsx
+++ src/components/Presets/Presets.jsx
@@ -1,5 +1,6 @@
 
 import react, {useState, useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import axios from 'axios'
 function Presets () {
 
@@ -8,6 +9,7 @@ function Presets () {
     useEffect(() => {
         getPresets()
     }, [])
+
     const getPresets = () => {
         axios({
             method: 'GET', 
@@ -15,19 +17,26 @@ function Presets () {
         }).then(res => {
             console.log('response is', res.data);
             setPresetList(res.data)
-            console.log(presetList);
         }).catch(err => {
             console.log('get err', err);
         })
         
+    }
+
+    const deletePreset = (id) => {
+        console.log('id is', id);
     }
     return(
         <>
         <h1>this is a presets page</h1>
         <h2>You can save and load presets over here</h2>
             <img src="https://i1.sndcdn.com/artworks-000191633248-ye4tjs-t500x500.jpg"/>
-        
-
+            
+        <ul>
+            {presetList.map(preset => (
+                <li key={preset.id}>{preset.name} <button>load</button> <button onClick={() => deletePreset(preset.id)}>delete</button></li>
+            ))}
+        </ul>
         </>
     )
 }
