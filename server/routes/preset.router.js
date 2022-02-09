d14b390a816a161bfd0d75f6a0656250b08530e7
Toivo Hannigan <THannigan046@gmail.com>
2022-02-09 10:43:01 -0600

    posting to database

--- server/routes/preset.router.js
+++ server/routes/preset.router.js
@@ -16,7 +16,24 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   // POST route code here
   console.log('made it to server', req.body);
-  
+  const name = req.body.presetName
+  const notes = req.body.notes
+  const userId = req.body.userId
+
+  let queryParams = [name, notes, userId]
+  const queryText = 
+  `INSERT INTO preset (name, notes, user_id)
+  VALUES ($1, $2, $3)
+  `;
+  pool.query(queryText, queryParams)
+    .then((result) => {
+      console.log('query success');
+      res.sendStatus(201)
+    })
+    .catch((error) => {
+      console.log('query error', error);
+      res.sendStatus(500)
+    })
 });
 
 router.put('/', (req, res) => {
