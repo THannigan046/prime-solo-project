a81f81edacc11a157774104b8717d21f4091e146
Toivo Hannigan <THannigan046@gmail.com>
2022-02-14 09:50:55 -0600

    more tests, more bugFixes

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -243,7 +243,7 @@ function Play() {
             type: 'FETCH_ACTIVE_PRESET', 
             payload: id
         })
-        axios.get(`api/preset/load/${id}`)
+        axios.get(`api/preset/${id}`)
         .then((res) => {
             console.log('response is', res.data);
             dispatch({
